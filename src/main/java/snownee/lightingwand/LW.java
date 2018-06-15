2c4afbcf6717d503d554981e64c752cdd973ad92
Snownee <1850986885@qq.com>
2018-06-15 21:47:57 +0800

    Bump version to 1.0.0

--- src/main/java/snownee/lightingwand/LW.java
+++ src/main/java/snownee/lightingwand/LW.java
@@ -14,7 +14,7 @@ public class LW
 {
     public static final String MODID = "lightingwand";
     public static final String NAME = "Lighting Wand";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "@VERSION_INJECT@";
     public static final String GUI_FACTORY = "snownee.lightingwand.common.GuiFactory";
 
     private static final LW INSTANCE = new LW();
