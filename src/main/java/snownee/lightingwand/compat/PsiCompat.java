bf342fc7b4c9102886cca56b6f308abf0f9866d0
Snownee <1850986885@qq.com>
2018-07-18 21:25:49 +0800

    Added Magical Psi compat

--- src/main/java/snownee/lightingwand/compat/PsiCompat.java
+++ src/main/java/snownee/lightingwand/compat/PsiCompat.java
@@ -1,5 +1,8 @@
 package snownee.lightingwand.compat;
 
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.Loader;
+import snownee.lightingwand.LW;
 import vazkii.psi.api.PsiAPI;
 import vazkii.psi.common.lib.LibPieceGroups;
 
@@ -7,7 +10,15 @@ public class PsiCompat
 {
     public static void init()
     {
-        PsiAPI.registerSpellPieceAndTexture("trickConjureLightInvisible", PieceTrickConjureLightInvisible.class);
+        PsiAPI.registerSpellPiece("trickConjureLightInvisible", PieceTrickConjureLightInvisible.class);
+
+        String textureName = "trick_conjure_light_invisible";
+        if (Loader.isModLoaded("magipsi"))
+        {
+            textureName = "magical_" + textureName;
+        }
+        PsiAPI.simpleSpellTextures.put("trickConjureLightInvisible", new ResourceLocation(LW.MODID, String.format("textures/spell/%s.png", textureName)));
+
         PsiAPI.addPieceToGroup(PieceTrickConjureLightInvisible.class, LibPieceGroups.BLOCK_CONJURATION, false);
     }
 }
