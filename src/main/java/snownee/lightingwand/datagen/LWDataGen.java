8e08f42da73bd374c4e00bb4f8383ce86a06fcc1
SettingDust <settingdust@gmail.com>
2026-05-14 19:29:32 +0800

    fix: wrong id for language provider

--- src/main/java/snownee/lightingwand/datagen/LWDataGen.java
+++ src/main/java/snownee/lightingwand/datagen/LWDataGen.java
@@ -2,6 +2,7 @@ package snownee.lightingwand.datagen;
 
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import snownee.kiwi.datagen.KiwiLanguageProvider;
+import snownee.lightingwand.LW;
 
 
 public final class LWDataGen {
@@ -11,6 +12,6 @@ public final class LWDataGen {
 	public static void gatherData(GatherDataEvent.Client event) {
 		event.createProvider(LWItemTagProvider::new);
 		event.createProvider(LWRecipeProvider.Runner::new);
-		event.createProvider(KiwiLanguageProvider::new);
+		event.createProvider((output, lookupProvider) -> new KiwiLanguageProvider(output, LW.ID, lookupProvider));
 	}
 }
