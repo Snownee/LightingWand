28a36bc409c10594f7d2f610ca54bed2f5ceb6bd
SettingDust <settingdust@gmail.com>
2026-05-14 19:24:05 +0800

    feat: add missing language datagen

--- src/main/java/snownee/lightingwand/datagen/LWDataGen.java
+++ src/main/java/snownee/lightingwand/datagen/LWDataGen.java
@@ -1,6 +1,7 @@
 package snownee.lightingwand.datagen;
 
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import snownee.kiwi.datagen.KiwiLanguageProvider;
 
 
 public final class LWDataGen {
@@ -10,5 +11,6 @@ public final class LWDataGen {
 	public static void gatherData(GatherDataEvent.Client event) {
 		event.createProvider(LWItemTagProvider::new);
 		event.createProvider(LWRecipeProvider.Runner::new);
+		event.createProvider(KiwiLanguageProvider::new);
 	}
 }
